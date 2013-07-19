f5ce08863c64b866dc84254f4d9e4f069f8200b4
mohuanlanjing <mohuanlanjing@163.com>
2013-07-19 14:01:28 +0800

    * pro12 modiflying

--- problems/MyAlg.py
+++ problems/MyAlg.py
@@ -21,16 +21,37 @@ def is_prime(num):
         i += 1
     return True
 
-# return the first num prime list
-def primelist(num):
-    plist = []
+#make a big prime list below limit
+def big_prime(limit=100000000):
+    blist = [1] * (limit+1)
+    blist[0] = 0
+    blist[1] = 0
+    max = int(sqrt(limit))
+    j = 2
     i = 2
-    while True:
-        if i>2 and i%2 == 0:
-            i += 1
+    while i*j<=limit:
+        blist[i*j] = 0
+        j = j + 1
+    j = 2
+    for i in range(3,max+1,2):
+        if blist[i] == 0:
             continue
         if is_prime(i):
-            plist.append(i)
-        if len(plist) == num:
-            return plist
-        i += 1
+            while i < max and i*j <= limit:
+                blist[i*j] = 0
+                j += 1
+        j = 2
+    return blist
+    
+# return the first num prime list
+def primelist(num):
+    bp = big_prime(num)
+    pl = []
+    count = 0
+    for i in bp:
+        if i:
+            pl.append(count)
+        count = count + 1
+    return pl
+
+print primelist(1000000)
--- problems/pro12.py
+++ problems/pro12.py
@@ -2,14 +2,14 @@
 """
 What is the value of the first triangle number to have over five hundred divisors?
 """
-from MyAlg import primelist,is_prime
+from MyAlg import primelist,is_prime,big_prime
 
 # get the numst triangle number
 def get_triangle(num):
     return sum(range(num+1))
 
-def short_division(num):
-    if is_prime(num):
+def short_division(num, bp):
+    if bp[num]: 
         return [1]
     k = 1
     plist = primelist(k)
@@ -28,7 +28,7 @@ def short_division(num):
             plist = primelist(k)
         num = num/plist[i]
         sdlist.append(plist[i])
-        if is_prime(num):
+        if bp[num]:
             sdlist.append(num)
             break
         i = 0
@@ -37,9 +37,10 @@ def short_division(num):
 if __name__ == '__main__':
     i = 2
     tn = 1
+    bp = big_prime()
     while True:
         num = get_triangle(i)
-        sdlist = short_division(num)
+        sdlist = short_division(num, bp)
         l = set(sdlist)
         for j in l:
             tn *= sdlist.count(j)+1
