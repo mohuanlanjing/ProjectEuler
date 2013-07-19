55127cd81ddb1048e4a0917f9815a1bcc5a76c78
mohuanlanjing <mohuanlanjing@163.com>
2013-07-19 14:16:21 +0800

    * pro12 change successfully

--- problems/pro12.py
+++ problems/pro12.py
@@ -8,24 +8,14 @@ from MyAlg import primelist,is_prime,big_prime
 def get_triangle(num):
     return sum(range(num+1))
 
-def short_division(num, bp):
+def short_division(num, bp, plist):
     if bp[num]: 
         return [1]
-    k = 1
-    plist = primelist(k)
     sdlist = []
     i = 0
-
     while i < len(plist):
         while num%plist[i] != 0:
-            if plist[i] == plist[-1]:
-                k = k+1
-                plist = primelist(k)
             i += 1
-        
-        if plist[i] == plist[-1]:
-            k = k+1
-            plist = primelist(k)
         num = num/plist[i]
         sdlist.append(plist[i])
         if bp[num]:
@@ -38,9 +28,10 @@ if __name__ == '__main__':
     i = 2
     tn = 1
     bp = big_prime()
+    plist = primelist(1000000)
     while True:
         num = get_triangle(i)
-        sdlist = short_division(num, bp)
+        sdlist = short_division(num, bp, plist)
         l = set(sdlist)
         for j in l:
             tn *= sdlist.count(j)+1
