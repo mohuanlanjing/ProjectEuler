5e76f97a033cc30ddcd00e6f954de224c12e428a
mohuanlanjing <mohuanlanjing@163.com>
2013-07-23 12:05:48 +0800

    * pro21 finish

---
+++ problems/pro20.py
@@ -0,0 +1,6 @@
+#coding: utf-8
+"""
+Find the sum of the digits in the number 100!
+"""
+from math import factorial
+print sum([int(i) for i in str(factorial(100))])
---
+++ problems/pro21.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+"""
+Evaluate the sum of all the amicable numbers under 10000.
+"""
+
+# get the proper divisor list
+def sum_proper_divisors_list(num, option='sum'):
+    proper_divisors_list = [i for i in range(1, int(num/2)) if not num%i]
+    return sum(proper_divisors_list)
+
+if __name__ == '__main__':
+    s = 0 
+    for a in range(2,10000):
+        da = sum_proper_divisors_list(a)
+        if da == 1:
+            continue
+        db = sum_proper_divisors_list(da)
+        if a == db and a != da:
+            s += a
+
+print s
+
