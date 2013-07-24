158b97328a26c8119e9200f5f19eecb7dc809a02
mohuanlanjing <mohuanlanjing@163.com>
2013-07-24 16:54:41 +0800

    * pro26 not finish

---
+++ problems/pro26.py
@@ -0,0 +1,12 @@
+#coding: utf-8
+"""
+Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+"""
+
+def long_division(num):
+    if not isinstance(num, float):
+        num = float(num)
+    
+    
+        
+
